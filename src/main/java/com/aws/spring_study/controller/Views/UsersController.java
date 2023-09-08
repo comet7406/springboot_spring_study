32bdd4948d652aa05500154c85aa34118e29213d
comet7406 <jsp7406@naver.com>
2023-09-08 17:25:32 +0900

    feat: "0908_오후(등록하기,수정하기,삭제하기)"

--- src/main/java/com/aws/spring_study/controller/Views/UsersController.java
+++ src/main/java/com/aws/spring_study/controller/Views/UsersController.java
@@ -1,13 +1,15 @@
 package com.aws.spring_study.controller.Views;
 
+import com.aws.spring_study.dto.ModifyUserReqDto;
 import com.aws.spring_study.dto.RegisterUserReqDto;
+import com.aws.spring_study.entity.User;
 import com.aws.spring_study.repository.UserMappers;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UsersController {
@@ -17,9 +19,38 @@ public class UsersController {
 
     @CrossOrigin
     @PostMapping("/user")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterUserReqDto registerUserReqDto) {
+    public ResponseEntity<Integer> registerUser(
+            @RequestBody RegisterUserReqDto registerUserReqDto) {
 
         Integer count = userMappers.saveUser(registerUserReqDto);
+
+        return ResponseEntity.ok().body(count);
+    }
+
+    @CrossOrigin
+    @GetMapping("/user/list")
+    public ResponseEntity<List<User>> userListAll() {
+
+        return ResponseEntity.ok().body(userMappers.getUserListAll());
+    }
+
+    @CrossOrigin
+    @PutMapping("/users/{userId}")
+    public ResponseEntity<Integer> modifyUser(
+            @PathVariable int userId,
+            @RequestBody ModifyUserReqDto modifyUserReqDto) {
+
+        Integer count = userMappers.updateUser(modifyUserReqDto);
+
+        System.out.println(userId);
+        System.out.println(modifyUserReqDto);
+        return ResponseEntity.ok().body(count);
+    }
+
+    @CrossOrigin
+    @DeleteMapping("/users/{userId}")
+    public ResponseEntity<Integer> deleteUser(@PathVariable int userId) {
+        Integer count = userMappers.deleteUser(userId);
 
         return ResponseEntity.ok().body(count);
     }
--- src/main/java/com/aws/spring_study/repository/UserMappers.java
+++ src/main/java/com/aws/spring_study/repository/UserMappers.java
@@ -1,9 +1,17 @@
 package com.aws.spring_study.repository;
 
+import com.aws.spring_study.dto.ModifyUserReqDto;
 import com.aws.spring_study.dto.RegisterUserReqDto;
+import com.aws.spring_study.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserMappers {
     public Integer saveUser(RegisterUserReqDto registerUserReqDto);
+    public List<User> getUserListAll();
+    public Integer updateUser(ModifyUserReqDto modifyUserReqDto);
+    public Integer deleteUser(Integer userId);
+
 }
